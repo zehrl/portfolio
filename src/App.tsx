135b9342521662e1931e88cb61f9b40d3f4fdc84
Logan Zehr <zehrl315@outlook.com>
2024-10-17 13:10:28 -0700

    React create app + header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import pictureOfMe from './assets/logan.jpeg'
+import { ReactComponent as LinkedInIcon } from './assets/linkedin-icon.svg';
+import { ReactComponent as GithubIcon } from './assets/github-icon.svg';
+import { ReactComponent as EmailIcon } from './assets/email-icon.svg';
+import { ReactComponent as DownloadIcon } from './assets/download-icon.svg';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="
+      min-h-screen max-w-xl mx-auto
+      bg-primary
+      px-4
+      text-dark
+    ">
+
+      <Header></Header>
+
+      {/* Navbar */}
+
+      {/* SectionTitle */}
+      {/* WorkExperience */}
+      {/* WorkExperience */}
+      {/* WorkExperience */}
+
+      {/* SectionTitle */}
+      {/* Education */}
+      {/* Education */}
+
+      {/* SectionTitle */}
+      {/* Certification */}
+      {/* Certification */}
+      {/* Certification */}
+
+      {/* TakeMeBackUpBtn */}
+
+      {/* Footer */}
+
     </div>
+
   );
+}
+
+const Header = () => {
+  return (
+    <header className="
+      flex flex-col items-center
+      pt-24 pb-2 gap-2
+    ">
+      <div className="
+        flex flex-col items-center
+        gap-2
+      ">
+
+        <img alt="Logan Zehr" src={pictureOfMe} className="
+          w-36 h-36
+          rounded-3xl border border-dark
+          drop-shadow-flat
+        "/>
+
+        <div className="text-center">
+          <p className="
+            text-3xl font-medium
+          ">Logan Zehr</p>
+
+          <p>Software Engineer</p>
+        </div>
+
+      </div>
+
+      <div className='
+        flex flex-row items-center
+      '>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/logan-j-zehr/" className='p-2'>
+          <LinkedInIcon></LinkedInIcon>
+        </a>
+
+        <a target="_blank" rel="noopener noreferrer" href="https://www.github.com/zehrl" className='p-2'>
+          <GithubIcon></GithubIcon>
+        </a>
+
+        <a target="_blank" rel="noopener noreferrer" href="mailto:zehrl315@outlook.com" className='p-2'>
+          <EmailIcon></EmailIcon>
+        </a>
+
+        <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1e9qUtyy_4BWa2A-cZPNxOQM8le_cFsJY/view?usp=sharing"
+          className='
+            flex flex-row items-center
+            m-2
+            px-2
+            py-1
+            gap-1
+            bg-secondary
+            rounded-3xl border border-dark
+            drop-shadow-flat
+        '><DownloadIcon className='inline'></DownloadIcon><span className='font-serif text-xs'>Resume</span></a>
+      </div>
+    </header >
+  )
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Inter', 'Arial', 'sans-serif'],
+      'serif': ['Aleo', 'Times New Roman', 'serif']
+    },
+    colors: {
+      'primary': '#EAE6D8',
+      'secondary': '#C2E2CA',
+      'dark': '#233735'
+    },
+    extend: {
+      dropShadow: ({ theme }) => ({
+        'flat': `0px 4px 0px ${theme('colors.dark')}`
+      })
+    },
+  },
+  plugins: [],
+}
